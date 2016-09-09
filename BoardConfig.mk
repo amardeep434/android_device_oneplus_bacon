9810acc827f61d98ba6247516c9ff180edebed19
Amardeep Arora <amardeep434@gmail.com>
2016-09-09 08:52:19 +0000

    Test tc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -51,12 +51,10 @@ BOARD_DTBTOOL_ARGS := -2
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := bacon_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
-#Kernel Toolchain
-KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
-TARGET_GCC_VERSION_ARM:=KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
 #Rom Toolchain
-TARGET_GCC_VERSION_EXP:=4.9-uber
+TARGET_GCC_VERSION_EXP:= 4.9-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
